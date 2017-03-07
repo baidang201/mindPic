c6ff00ddf6f383c04b0017609929ddd8817c8318
liyh <jinrishouji@sina.com>
2017-03-07 14:32:17 +0800

    首次提交

---
+++ js编程分享.mm
@@ -0,0 +1,73 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1488250203735" ID="ID_1036808187" MODIFIED="1488250280935" TEXT="js&#x7f16;&#x7a0b;&#x5206;&#x4eab;">
+<node CREATED="1488250214939" ID="ID_1466923304" MODIFIED="1488250369709" POSITION="right" TEXT="&#x8c03;&#x8bd5;">
+<node CREATED="1488250371530" ID="ID_367952840" MODIFIED="1488254335356" TEXT="js&#x6587;&#x4ef6;&#xff0c;&#x6dfb;&#x52a0;&#x6253;&#x5370;&#x8bed;&#x53e5; console.log()">
+<node CREATED="1488250934785" ID="ID_1666483527" MODIFIED="1488254201377" TEXT="&#x5728;chrome&#x7684;console&#x6807;&#x7b7e;&#x4e0b;&#x67e5;&#x770b;">
+<node CREATED="1488251179210" ID="ID_1262439476" MODIFIED="1488251179210" TEXT=""/>
+</node>
+</node>
+<node CREATED="1488254206333" ID="ID_413197813" MODIFIED="1488254209093" TEXT="chrome&#x7684;console&#x6807;&#x7b7e;&#x4e0b;">
+<node CREATED="1488254209093" ID="ID_1294848182" MODIFIED="1488254216545" TEXT="&#x4ea4;&#x4e92;&#x5f0f;&#x8f93;&#x5165;&#x8f93;&#x51fa;">
+<node CREATED="1488254416660" ID="ID_850781132" MODIFIED="1488254416660" TEXT=""/>
+</node>
+</node>
+<node CREATED="1488250727081" ID="ID_1067858062" MODIFIED="1488254186908" TEXT="Live Reload chrome&#x63d2;&#x4ef6;&#xff08;&#x670d;&#x52a1;&#x7aef;&#x66f4;&#x6539;html js&#x540e;&#xff0c;chrome&#x81ea;&#x52a8;&#x4e0b;&#x8f7d;&#x5237;&#x65b0;&#xff09;">
+<node CREATED="1488250860418" ID="ID_149318642" MODIFIED="1488250864035" TEXT=" A Deveploer Tools for Live Reload Page Resources(html,css,js) without F5"/>
+</node>
+</node>
+<node CREATED="1488250294753" ID="ID_1394005587" MODIFIED="1488251241358" POSITION="right" TEXT="js&#x4ee3;&#x7801;&#x7ed3;&#x6784;">
+<node CREATED="1488251242433" ID="ID_742203080" MODIFIED="1488253422736" TEXT="html&#x6587;&#x4ef6;&#x5185;&#x5d4c;&#x5165;">
+<node CREATED="1488251308362" ID="ID_1317938142" MODIFIED="1488251353305">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;script type=&quot;text/javascript&quot;&gt;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;function onLogin(){&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+    <p>
+      var url = '/onlogin';&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;return false;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;}&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;
+    </p>
+    <p>
+      &#160;&lt;/script&gt;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1488251246522" ID="ID_455912000" MODIFIED="1488251258787" TEXT="js&#x540e;&#x7f00;&#x72ec;&#x7acb;&#x6210;&#x4e00;&#x4e2a;&#x6587;&#x4ef6;"/>
+</node>
+<node CREATED="1488251365346" ID="ID_851147749" MODIFIED="1488251369030" POSITION="left" TEXT="&#x6ce8;&#x610f;&#x4e8b;&#x9879;">
+<node CREATED="1488251370386" ID="ID_1390879018" MODIFIED="1488251486038" TEXT="1 &#x7981;&#x6b62;&#x591a;&#x6b21;&#x5f15;&#x5165;&#x540c;&#x4e00;&#x4e2a;&#x5e93;&#xff0c;&#x4e0d;&#x540c;&#x7248;&#x672c;&#x7684;">
+<node CREATED="1488251549450" ID="ID_125435142" MODIFIED="1488251555136" TEXT="&#x4f7f;&#x7528;&#x4e2d;&#x7684;bug"/>
+</node>
+</node>
+<node CREATED="1488251418842" ID="ID_1630245222" MODIFIED="1488251423326" POSITION="left" TEXT="&#x5f15;&#x5165;js">
+<node CREATED="1488251443178" ID="ID_404918626" MODIFIED="1488251446172" TEXT="&lt;script src=&quot;../static/js/jquery.js&quot;&gt;&lt;/script&gt;"/>
+</node>
+<node CREATED="1488251509402" ID="ID_826355551" MODIFIED="1488251537230" POSITION="right" TEXT="&#x5feb;&#x901f;&#x589e;&#x5220;&#x6539; grid&#x7684;js&#x5e93;">
+<node CREATED="1488251538762" ID="ID_994610330" MODIFIED="1488251541278" TEXT="easyui"/>
+</node>
+<node CREATED="1488253833204" ID="ID_758014033" MODIFIED="1488253838836" POSITION="left" TEXT="&#x4f18;&#x5316;">
+<node CREATED="1488253838837" ID="ID_165861272" MODIFIED="1488253863688" TEXT="&#x5220;&#x9664;&#x65e0;&#x6548;&#x7684;css&#x5f15;&#x5165;&#xff0c;&#x524d;&#x7aef;&#x5c31;&#x4e0d;&#x7528;&#x4e0b;&#x8f7d;&#x4e86;">
+<node CREATED="1488253864812" ID="ID_1788775746" MODIFIED="1488253961864" TEXT="&#x53f3;&#x952e;&#x68c0;&#x67e5;-&#x300b;&#x70b9;&#x51fb;&#x201c;Audits&#x201d; tab&#x6807;&#x7b7e;-&#x300b;&#x9009;&#x4e2d;&#x201c;Web Page Performance&#x201d;&#x590d;&#x9009;-&#x300b; &#x70b9;&#x51fb;&#x201c;Run&#x201d;"/>
+</node>
+</node>
+<node CREATED="1488867851882" ID="ID_530592542" MODIFIED="1488867857655" POSITION="right" TEXT="&#x5de5;&#x5177;">
+<node CREATED="1488867857656" ID="ID_1184124674" MODIFIED="1488867861049" TEXT="jquery">
+<node CREATED="1488867863332" ID="ID_1926911411" MODIFIED="1488867874874" TEXT="&#x6587;&#x7ae0; &#x300a; jQuery&#x5feb;&#x901f;&#x4e0a;&#x624b;&#xff1a;&#x5199;jQuery&#x548c;&#x76f4;&#x63a5;&#x5199;JS&#x7684;&#x533a;&#x522b;&#x300b;"/>
+</node>
+</node>
+</node>
+</map>
